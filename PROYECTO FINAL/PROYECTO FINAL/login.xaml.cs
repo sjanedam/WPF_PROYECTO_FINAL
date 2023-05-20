e6ea7cc29892d1d1d1375407f2551af1034d2e0d
jane <99421029+sjanedam@users.noreply.github.com>
2023-05-20 20:15:40 +0200

    Más cambios

--- PROYECTO FINAL/PROYECTO FINAL/login.xaml.cs
+++ PROYECTO FINAL/PROYECTO FINAL/login.xaml.cs
@@ -14,9 +14,6 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 
-//using Console = System.Console;
-//using System.Data.Odbc;
-
 namespace PROYECTO_FINAL
 {
     /// <summary>
